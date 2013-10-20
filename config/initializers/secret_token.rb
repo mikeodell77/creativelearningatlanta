adbc7a83cd60f21971f3a7c7f672988f8f228fd5
ianmdrennan <drennan.m.ian@gmail.com>
2013-10-20 15:27:17 -0400

    first theme pass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreativeLearningAtlanta::Application.config.secret_key_base = '5bb63d7f90136e38dca9ec2676a23eec16d72a146155fbb4a49e40cf1c2abd36b24bd817621ce5cc91fb861c67fc38cdcd73d975763863e6345f6c5aac07bc63'
